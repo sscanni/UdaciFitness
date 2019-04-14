f7eb606f41a12233c32d5e8ba4861431e9dc1300
sscanni <sscanni@comcast.net>
2019-04-13 23:30:18 -0400

    Completed Styling Changes

--- App.js
+++ App.js
@@ -4,13 +4,14 @@ import AddEntry from './components/addentry'
 import { createStore } from 'redux'
 import { Provider } from 'react-redux'
 import reducer from './reducers'
+import History from './components/history'
 
 export default class App extends React.Component {
   render() {
     return (
       <Provider store={createStore(reducer)}>
         <View style={{flex: 1}}>
-          <AddEntry />
+          <History />
         </View>
       </Provider>
     )
